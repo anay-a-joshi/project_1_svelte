1232be6ac8e2e5580e371e48b06135ec4d9425cd
ANAY JOSHI <JOSHI2AN@MAIL.UC.EDU>
2024-09-22 21:21:11 -0400

    Refactor user prompt message for skill selection in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -73,7 +73,7 @@
         retrieveJournalData();  
 
         setTimeout(() => {
-            skill = prompt(`What skill would you like to work on today, ${user.Name}?`) || 'Skill';  // Fallback to 'Skill' if input is empty
+            skill = prompt(`Hi ${user.Name}, what specific skill would you like to focus on improving today?`) || 'Skill';  // Fallback to 'Skill' if input is empty
     }, 100);
     };
 
